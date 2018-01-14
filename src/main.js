49b4a86ac399b278523a9e10ccc30946b90f9c6c
sheaivey <ivey.shea@gmail.com>
2018-01-14 11:52:59 -0800

    only watch for source changes when the window is open.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 'use strict';
-let rootWindow = false;
 
 function startApplication() {
     var applicationStartTime = new Date().getTime();
@@ -11,57 +10,65 @@ function startApplication() {
             minHeight: 600
         }
     }, function (createdWindow) {
-        rootWindow = createdWindow;
         createdWindow.onClosed.addListener(function () {
             // do something when the app closes?
         });
+
+        // Start hot reload when the app is open.
+        startHotReload();
     });
 }
 
 chrome.app.runtime.onLaunched.addListener(startApplication);
 
-chrome.management.getSelf (self => {
-  if (self.installType === 'development') {
-    // set up hot reloader to watch for file changes
-    const filesInDirectory = dir => new Promise (resolve =>
-      dir.createReader ().readEntries (entries =>
-        Promise.all (entries.filter (e => e.name[0] !== '.').map (e =>
-          e.isDirectory ? filesInDirectory (e) : new Promise (resolve => e.file (resolve))
-        ))
-        .then (files => [].concat (...files))
-        .then (resolve)
-      )
-    );
+function startHotReload() {
+  chrome.management.getSelf(self => {
+    if (self.installType === 'development') {
+      // set up hot reloader to watch for file changes
+      const filesInDirectory = dir => new Promise(resolve =>
+        dir.createReader().readEntries(entries =>
+          Promise.all(entries.filter(e => e.name[0] !== '.').map(e =>
+            e.isDirectory ? filesInDirectory(e) : new Promise(resolve => e.file(resolve))
+          ))
+          .then(files => [].concat(...files))
+          .then(resolve)
+        )
+      );
 
-    const timestampForFilesInDirectory = dir => filesInDirectory (dir).then (files => files.map (f => f.name + f.lastModifiedDate).join ());
+      const timestampForFilesInDirectory = dir => filesInDirectory(dir).then(files => files.map(f => f.name + f.lastModifiedDate).join());
 
-    const reload = () => {
-      if(rootWindow) {
-        // reload the entire app
-        // console.log('hot reloading app...');
-        // chrome.runtime.reload();
+      const reload = () => {
+        const rootWindow = chrome.app.window.getAll()[0];
+        if(rootWindow) {
+          // reload the entire app
+          // console.log('hot reloading app...');
+          // chrome.runtime.reload();
 
-        var frame = rootWindow.contentWindow.document.getElementById("rootWindow");
-        if(frame) {
-          console.log('hot reloading app...');
-          frame.contentWindow.window.location.reload(true);
-          // start watching for changes again.
-          chrome.runtime.getPackageDirectoryEntry (dir => watchChanges (dir));
+          var frame = rootWindow.contentWindow.document.getElementById("rootWindow");
+          if(frame) {
+            console.log('hot reloading app...');
+            frame.contentWindow.window.location.reload(true);
+            // start watching for changes again.
+            chrome.runtime.getPackageDirectoryEntry(dir => watchChanges (dir));
+          }
         }
       }
-    }
 
-    const watchChanges = (dir, lastTimestamp) => {
-      timestampForFilesInDirectory (dir).then (timestamp => {
-        if (!lastTimestamp || (lastTimestamp === timestamp)) {
-          setTimeout (() => watchChanges (dir, timestamp), 1000); // retry after 1s
-        } else {
-          reload ();
+      const watchChanges = (dir, lastTimestamp) => {
+        const rootWindow = chrome.app.window.getAll()[0];
+        if(rootWindow) {
+          timestampForFilesInDirectory(dir).then (timestamp => {
+            if (!lastTimestamp || (lastTimestamp === timestamp)) {
+              setTimeout(() => watchChanges(dir, timestamp), 1000); // retry after 1s
+            } else {
+              reload();
+            }
+          });
         }
-      });
-    }
+      }
 
-    // start watching...
-    chrome.runtime.getPackageDirectoryEntry (dir => watchChanges (dir));
-  }
-});
+      // start watching...
+      chrome.runtime.getPackageDirectoryEntry(dir => watchChanges(dir));
+    }
+  });
+};
