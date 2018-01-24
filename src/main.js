f794a9499b4f9ea122a85f5fb88f30d449e0e90f
Shea Ivey <sheaivey@users.noreply.github.com>
2018-01-24 09:30:20 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,54 +13,7 @@ function startApplication() {
         createdWindow.onClosed.addListener(function () {
             // do something when the app closes?
         });
-
-        // Start hot reload when the app is open.
-        startHotReload();
     });
 }
 
 chrome.app.runtime.onLaunched.addListener(startApplication);
-
-function startHotReload() {
-  chrome.management.getSelf(self => {
-    if (self.installType === 'development') {
-      // set up hot reloader to watch for file changes
-      const filesInDirectory = dir => new Promise(resolve =>
-        dir.createReader().readEntries(entries =>
-          Promise.all(entries.filter(e => e.name[0] !== '.').map(e =>
-            e.isDirectory ? filesInDirectory(e) : new Promise(resolve => e.file(resolve))
-          ))
-          .then(files => [].concat(...files))
-          .then(resolve)
-        )
-      );
-
-      const timestampForFilesInDirectory = dir => filesInDirectory(dir).then(files => files.map(f => f.name + f.lastModifiedDate).join());
-
-      const reload = () => {
-        const rootWindow = chrome.app.window.getAll()[0];
-        if(rootWindow) {
-          //reload the entire app
-          console.log('hot reloading app...');
-          chrome.runtime.reload();
-        }
-      }
-
-      const watchChanges = (dir, lastTimestamp) => {
-        const rootWindow = chrome.app.window.getAll()[0];
-        if(rootWindow) {
-          timestampForFilesInDirectory(dir).then (timestamp => {
-            if (!lastTimestamp || (lastTimestamp === timestamp)) {
-              setTimeout(() => watchChanges(dir, timestamp), 1000); // retry after 1s
-            } else {
-              reload();
-            }
-          });
-        }
-      }
-
-      // start watching...
-      chrome.runtime.getPackageDirectoryEntry(dir => watchChanges(dir));
-    }
-  });
-};
